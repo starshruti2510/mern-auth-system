0e6dee745e69f52abf2a44dc134bb7d937db31a0
student-shruti <shrutisharma87860@gmail.com>
2026-06-14 16:13:34 +0530

    Update login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -26,7 +26,7 @@ function Login() {
             return handleError('email and passwords are required')
         }
         try {
-            const url = "http://localhost:8080/auth/login";
+            const url = "https://mern-auth-system-api-peach.vercel.app/auth/login";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
