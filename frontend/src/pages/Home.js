123130eab9cd07020183c3b658300a1b52561eb6
student-shruti <shrutisharma87860@gmail.com>
2026-06-14 16:14:31 +0530

    Update home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -23,7 +23,7 @@ function Home() {
 
   const fetchProducts = async () => {
     try {
-      const url = "http://localhost:8080/products";
+      const url = "https://mern-auth-system-api-peach.vercel.app/products";
       const headers = {
         headers : {
           'Authorization': localStorage.getItem('token')
